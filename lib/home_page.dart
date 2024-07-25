91962eac938a74acfe92e9bbaa5ee6892f638ef1
37336 <pavana.n@tataelxsi.co.in>
2024-07-25 14:04:31 +0530

    counter app with bloc done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,72 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'cubit/counter_cubit.dart';
+import 'bloc/counter_bloc.dart';
+import 'bloc/counter_event.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
-    return BlocBuilder<CounterCubit, int>(
-        bloc: counterCubit,
-        builder: (context, counter) {
-          return Center(
-            child: Column(
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    return BlocBuilder<CounterBloc, int>(builder: (context, counter) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Counter:$counter',
+              style: const TextStyle(
+                  color: Colors.amber,
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'Counter:$counter',
-                  style: const TextStyle(
-                      color: Colors.amber,
-                      fontFamily: 'Pacifico',
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.amber),
-                        ),
-                        onPressed: () {
-                          counterCubit.increment();
-                        },
-                        child: const Text(
-                          'Increment',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                              fontFamily: 'SourceCodePro'),
-                        )),
-                    const SizedBox(
-                      width: 10.0,
+                ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.amber),
                     ),
-                    ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.amber),
-                        ),
-                        onPressed: () {
-                          counterCubit.decrement();
-                        },
-                        child: const Text(
-                          'Decrement',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                              fontFamily: 'SourceCodePro'),
-                        )),
-                  ],
+                    onPressed: () {
+                      counterBloc.add(CounterIncremented());
+                    },
+                    child: const Text(
+                      'Increment',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          fontFamily: 'SourceCodePro'),
+                    )),
+                const SizedBox(
+                  width: 10.0,
                 ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.amber),
+                    ),
+                    onPressed: () {
+                      counterBloc.add(CounterDecrement());
+                    },
+                    child: const Text(
+                      'Decrement',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          fontFamily: 'SourceCodePro'),
+                    )),
               ],
             ),
-          );
-        });
+          ],
+        ),
+      );
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mc_demoapp/bloc/counter_bloc.dart';
 
 import 'cubit/counter_cubit.dart';
 import 'home_page.dart';
@@ -11,8 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CounterCubit(),
+        ),
+        BlocProvider(
+          create: (context) => CounterBloc(),
+        ),
+      ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
