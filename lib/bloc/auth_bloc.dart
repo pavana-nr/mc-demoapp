a72790e7a003486730fe1dc4e4f7f0f0e83645ce
37336 <pavana.n@tataelxsi.co.in>
2024-07-26 15:22:11 +0530

    segregated the function

--- lib/bloc/auth_bloc.dart
+++ lib/bloc/auth_bloc.dart
@@ -6,34 +6,39 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>((event, emit) {
-      emit(AuthLoading());
-      try {
-        final email = event.email;
-        final password = event.password;
-
-        if (!isValidEmail(email)) {
-          return emit(AuthFailure('Invalid email address'));
-        }
-        if (password.length < 6) {
-          return emit(
-              AuthFailure('Password must be at least 6 characters long'));
-        }
-        emit(AuthSuccess(uid: '$email- $password'));
-      } catch (e) {
-        return emit(AuthFailure(e.toString()));
-      }
-    });
-    on<AuthLogoutRequested>((event, emit) async {
-      emit(AuthInitial());
-      try {
-        await Future.delayed(const Duration(seconds: 1));
-        emit(AuthInitial());
-      } catch (e) {
-        return emit(AuthFailure(e.toString()));
-      }
-    });
+    on<AuthLoginRequested>(_onAuthLoginRequested);
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
   }
+  void _onAuthLoginRequested(
+      AuthLoginRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final email = event.email;
+      final password = event.password;
+
+      if (!isValidEmail(email)) {
+        return emit(AuthFailure('Invalid email address'));
+      }
+      if (password.length < 6) {
+        return emit(AuthFailure('Password must be at least 6 characters long'));
+      }
+      emit(AuthSuccess(uid: '$email- $password'));
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(
+      AuthLogoutRequested event, Emitter<AuthState> emit) async {
+    emit(AuthInitial());
+    try {
+      await Future.delayed(const Duration(seconds: 1));
+      emit(AuthInitial());
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
+  }
+
   bool isValidEmail(String email) {
     final RegExp emailRegex =
         RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
